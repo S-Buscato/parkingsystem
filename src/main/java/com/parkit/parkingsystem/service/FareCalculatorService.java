be6acb71ccd17b9f1d57fc38c53b183222d33510
S-Buscato <sylvain.buscato@gmail.com>
2020-12-04 23:05:45 +0100

    fix duration calcul and add free first 30 min

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -1,5 +1,7 @@
 package com.parkit.parkingsystem.service;
 
+import java.sql.Date;
+
 import com.parkit.parkingsystem.constants.Fare;
 import com.parkit.parkingsystem.model.Ticket;
 
@@ -10,16 +12,17 @@ public class FareCalculatorService {
 			throw new IllegalArgumentException("Out time provided is incorrect:" + ticket.getOutTime().toString());
 		}
 
-		int inHour = ticket.getInTime().getHours();
-		int outHour = ticket.getOutTime().getHours();
+		Long inHour = ticket.getInTime().getTime();
+		Long outHour = ticket.getOutTime().getTime();
 
 		// TODO: Some tests are failing here. Need to check if this logic is correct
-		int duration = outHour - inHour;
+		long duration = (outHour - inHour)/1000 / 60 / 60;
+		System.out.println("duration : " +  duration);
 		double price = 0;
-
+		int free30Min = 30;
 		switch (ticket.getParkingSpot().getParkingType()) {
 		case CAR: {
-			price = duration * Fare.CAR_RATE_PER_HOUR;
+			price = duration > free30Min ? duration * Fare.CAR_RATE_PER_HOUR / 60: 0;
 			if (ticket.isRegularCustomer()) {
 				price = price * Fare.DISCOUNT_FOR_REGULAR_CUSTOMER;
 				System.out.println("DISCOUNT !!!!!!!!!!" + (1 - Fare.DISCOUNT_FOR_REGULAR_CUSTOMER)* 100 + "%");
@@ -28,7 +31,7 @@ public class FareCalculatorService {
 			break;
 		}
 		case BIKE: {
-			price = duration * Fare.BIKE_RATE_PER_HOUR;
+			price = duration > free30Min ? duration * Fare.BIKE_RATE_PER_HOUR / 60 : 0;
 			if (ticket.isRegularCustomer()) {
 				price = price * Fare.DISCOUNT_FOR_REGULAR_CUSTOMER;
 				System.out.println("DISCOUNT !!!!!!!!!!" + (1 - Fare.DISCOUNT_FOR_REGULAR_CUSTOMER)* 100 + "%");
@@ -40,4 +43,6 @@ public class FareCalculatorService {
 			throw new IllegalArgumentException("Unkown Parking Type");
 		}
 	}
+	
+	
 }
